ebf1013fe8c0da8af56399f776573821cc53267d
Matthew Donahue <matthew.rb.donahue@gmail.com>
2020-11-09 04:10:15 -0500

    Have the matrix right but not plotted

--- Tut13Answers.R
+++ Tut13Answers.R
@@ -11,8 +11,23 @@ pop <- matrix(data=NA, nrow=length(time),ncol=3)
 pop[,1] <- 1:length(time)
 pop[1,2] <- No
 pop[1,3] <- 1
+
 for (i in 2:length(time)){
-    pop[i,2] <- pop[i-1,2]+(rnn*pop[i-1,2]*(1-(pop[i-1,2]+pop[i-1,3])/K))
-    pop[i,3] <- pop[i-1,3]+(rnn*pop[i-1,3]*(1-(pop[i-1,2]+pop[i-1,3])/K))
+  pop[i,2] <- pop[i-1,2]+(rnn*pop[i-1,2]*(1-(pop[i-1,2]+pop[i-1,3])/K))
+  pop[i,3] <- pop[i-1,3]+(rmn*pop[i-1,3]*(1-(pop[i-1,2]+pop[i-1,3])/K))
 }
+
+for (i in 2:length(time)){
+  if(i < which(K - (pop[,2]+pop[,3])<100)[1]){
+    pop[i,2] <- pop[i-1,2]+(rnn*pop[i-1,2]*(1-(pop[i-1,2]+pop[i-1,3])/K))
+    pop[i,3] <- pop[i-1,3]+(rmn*pop[i-1,3]*(1-(pop[i-1,2]+pop[i-1,3])/K))
+  }
+  else{
+    pop[i,2] <- pop[i-1,2]+(rnt*pop[i-1,2]*(1-(pop[i-1,2]+pop[i-1,3])/K))
+    pop[i,3] <- pop[i-1,3]+(rmt*pop[i-1,3]*(1-(pop[i-1,2]+pop[i-1,3])/K))  
+  }
+}
+  
+
+
 rm(pop)
