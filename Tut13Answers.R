147c86af8d68160f0e340c3cf781a23bed1d2bef
Matthew Donahue <matthew.rb.donahue@gmail.com>
2020-11-09 03:32:04 -0500

    Got it to equilibrium pre treatment

---
+++ Tut13Answers.R
@@ -0,0 +1,18 @@
+setwd("C:/Users/matth/Desktop/Intro_to_Biocomputing/BioComp_Tutorial13")
+time <- 1:10000
+rnn <- .1
+rnt <- -.1
+rmn <- .1
+rmt <- .05
+
+K=1000000
+No=99
+pop <- matrix(data=NA, nrow=length(time),ncol=3)
+pop[,1] <- 1:length(time)
+pop[1,2] <- No
+pop[1,3] <- 1
+for (i in 2:length(time)){
+    pop[i,2] <- pop[i-1,2]+(rnn*pop[i-1,2]*(1-(pop[i-1,2]+pop[i-1,3])/K))
+    pop[i,3] <- pop[i-1,3]+(rnn*pop[i-1,3]*(1-(pop[i-1,2]+pop[i-1,3])/K))
+}
+rm(pop)
